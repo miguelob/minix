922d0abb61c7d1758adc00096ab047cf470f6641
miguelob <oleoblancomiguel@gmail.com>
2022-10-22 14:13:51 -0500

    added files and prints for debugging

--- minix/kernel/system/init_msgcounter.c
+++ minix/kernel/system/init_msgcounter.c
@@ -1,13 +1,14 @@
 #include "kernel/system.h"
-#include <minix/endpoint.h>
+ #include <minix/endpoint.h>
 
-/*===========================================================================*
- *                                init_msgcounter                                  *
- *===========================================================================*/
-static int msgcounter_value = 0;
+ /*===========================================================================*
+  *                                init_msgcounter                                  *
+  *===========================================================================*/
+ static int msgcounter_value = 0;
 
-int init_msgcounter(struct proc *caller_ptr, message *m_ptr)
-{
-    msgcounter_value = 0;
-    return (OK);
-}
+ int init_msgcounter(struct proc *caller_ptr, message *m_ptr)
+ {
+     printf("init_msgcounter executed from kernel side");
+     mscounter_value = 0;
+     return (OK);
+ }
--- minix/kernel/system/init_trapcounter.c
+++ minix/kernel/system/init_trapcounter.c
@@ -1,14 +1,15 @@
 #include "kernel/system.h"
 #include <minix/endpoint.h>
 
-/*===========================================================================*
- *                                init_trapcounter                                  *
- *===========================================================================*/
+ /*===========================================================================*
+  *                                init_trapcounter                                  *
+  *===========================================================================*/
 
-static int trapcounter_value = 0;
+ static int trapcounter_value = 0;
 
-int init_trapcounter(struct proc *caller_ptr, message *m_ptr)
-{
-    trapcounter_value = 0;
-    return (OK);
-}
+ int init_trapcounter(struct proc *caller_ptr, message *m_ptr)
+ {
+	 printf("init_trapcounter executed from kernel side");
+     trapcounter_value = 0;
+     return (OK);
+ }
